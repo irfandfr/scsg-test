df3d9238b596cc9c7dfddfc93b80cfaca02da1bb
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-27 13:54:49 +0700

    add fonts and global style

---
+++ src/component/fonts/fonts.tsx
@@ -0,0 +1,5 @@
+import { Roboto } from 'next/font/google'
+import { Poppins } from 'next/font/google'
+
+export const roboto = Roboto({ weight:['300','400','500','700'] , subsets: ['latin'] })
+export const poppins = Poppins({ weight:['300','400','500','700'] , subsets: ['latin'] })
