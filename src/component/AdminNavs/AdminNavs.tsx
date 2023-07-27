1df02f525ae08da48cc9afd094e2321589d0cefc
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-27 18:15:10 +0700

    finish mobile navigation header

--- src/component/AdminNavs/AdminNavs.tsx
+++ src/component/AdminNavs/AdminNavs.tsx
@@ -1,17 +1,31 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
 import BelaundryLogo from "../icons/BelaundryLogo/BelaundryLogo";
 import BurgerMenuIcon from "../icons/BurgerMenuIcon/BurgerMenuIcon";
 
 import styles from './adminnavs.module.scss'
+import LinkContainer from "./LinkContainer";
 
+
+const links = [
+  {link: '/secret/admin', title: 'Home',exact:true},
+  {link: '/secret/admin/products', title: 'Products'},
+  {link: '/secret/admin/sales', title: 'Sales'},
+]
 export default function AdminNavs(){
+  const [openNav, setOpen] = useState(false)
   return(
     <nav className={styles.adminNavs}>
       <div className={styles.linkNavs}>
         <Link href={'/secret/admin'}>
           <BelaundryLogo />
         </Link>
-        <BurgerMenuIcon />
+        <button onClick={() => setOpen(prev => !prev)} className='md-hide'>
+          <BurgerMenuIcon active={openNav} />
+        </button>
+        <LinkContainer links={links} open={openNav} />
       </div>
       <div className={styles.headerNav}></div>
     </nav>
---
+++ src/component/AdminNavs/LinkContainer.tsx
@@ -0,0 +1,59 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import UserIcon from "../icons/UserIcon/UserIcon";
+
+import styles from './adminnavs.module.scss'
+
+interface LinkContainerProp{
+  links : {
+    link: string
+    title: string
+    exact?:boolean
+  }[]
+  open: boolean
+}
+
+interface NavLinkProp{
+  link: string
+  title: string
+  active:boolean
+}
+
+const NavLink = ({link, title, active} : NavLinkProp) => {
+  return(
+    <Link href={link} className={`${styles.linkStyle} ${active ? styles.active : ''}`}>
+      {title}
+    </Link>
+  )
+}
+
+
+export default function LinkContainer({links, open} : LinkContainerProp){
+  const pathname = usePathname()
+
+  function isActiveLink(exact:boolean|undefined, link: string){
+    if(exact){
+      return link === pathname
+    }else{
+      return pathname.includes(link)
+    }
+  }
+
+  return(
+    <div className={`${styles.linkContainer} ${open ? styles.open : ''}`}>
+      <div className={styles.wrapper}>
+        <span className={styles.labelText}>Menu</span>
+        {
+          links.map(({link, title, exact},index) => 
+            <NavLink key={'link'+index} link={link} title={title} active={isActiveLink(exact,link)} />
+          )
+        }
+        <div className={`${styles.userLink} md-hide`} style={{marginTop: 'auto'}}>
+          <UserIcon className={styles.icon}/>
+          <span className={styles.username}>John Doe</span>
+        </div>
+      </div>
+    </div>
+  )
+}
