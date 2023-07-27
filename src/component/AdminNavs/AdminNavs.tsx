5d8c495706a9e26719955f7785774b04dec3b9dc
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-27 15:25:48 +0700

    add burger icon meny and stylesheet on nav

--- src/component/AdminNavs/AdminNavs.tsx
+++ src/component/AdminNavs/AdminNavs.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import BelaundryLogo from "../icons/BelaundryLogo";
+import BelaundryLogo from "../icons/BelaundryLogo/BelaundryLogo";
+import BurgerMenuIcon from "../icons/BurgerMenuIcon/BurgerMenuIcon";
+
+import styles from './adminnavs.module.scss'
 
 export default function AdminNavs(){
   return(
-    <nav>
-      <Link href={'/secret/admin'}>
-        <BelaundryLogo />
-      </Link>
+    <nav className={styles.adminNavs}>
+      <div className={styles.linkNavs}>
+        <Link href={'/secret/admin'}>
+          <BelaundryLogo />
+        </Link>
+        <BurgerMenuIcon />
+      </div>
+      <div className={styles.headerNav}></div>
     </nav>
   )
 }
