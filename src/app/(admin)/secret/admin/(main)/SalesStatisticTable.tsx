f745ccb1eda892fd8d64537284cd8ad9771bd4e1
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-28 22:52:56 +0700

    add ui for filtering sales chart

--- src/app/(admin)/secret/admin/(main)/SalesStatisticTable.tsx
+++ src/app/(admin)/secret/admin/(main)/SalesStatisticTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminCard from "@/component/AdminCard/AdminCard";
 import BarChart from "@/component/BarChart/BarChart";
 
@@ -8,9 +8,15 @@ import { ReportProps } from "@/component/types/types";
 
 import styles from './adminhome.module.scss'
 import { poppins } from "@/component/fonts/fonts";
+import SelectForm from "@/component/SelectForm/SelectForm";
 
-export default function SalesStatisticTable() {
+interface SalesStatisticsProp{
+  filterSales: {title: string, value: string}[]
+}
+
+export default function SalesStatisticTable({filterSales} : SalesStatisticsProp) {
   const [reports, setReports] = useState<ReportProps[]>([]);
+  const [filter, setFilter] = useState('week')
   useEffect(() => {
     AxiosGetWithToken("/platform/product/report").then((res) => {
       if (res.status === 200) {
@@ -20,10 +26,14 @@ export default function SalesStatisticTable() {
       }
     });
   }, []);
+
   return (
     <AdminCard className={styles.statisticTable}>
-      <h3 className={`${poppins.className} ${styles.title}`}>Product Sold</h3>
-      <hr />
+      <div style={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
+        <h3 className={`${poppins.className} ${styles.title}`}>Product Sold</h3>
+        <SelectForm options={filterSales} value={filter} onChange={(e) =>setFilter(e.target.value)}/>
+      </div>
+      <hr style={{marginTop: '17px'}}/>
       <div className={styles.tableContainer}>
         <BarChart title="Sales Report" datas={reports.map(report => {return {label: report.created_at, value: report.income, totalItem: report.total}})} />
       </div>
