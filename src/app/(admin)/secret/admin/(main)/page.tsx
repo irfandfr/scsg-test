b81a26a88c4d9140bcd6433beb7b2689b0dd6849
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-28 22:53:14 +0700

    add top sales component to home page

--- src/app/(admin)/secret/admin/(main)/page.tsx
+++ src/app/(admin)/secret/admin/(main)/page.tsx
@@ -1,10 +1,18 @@
 import SalesStatisticTable from "./SalesStatisticTable";
 import styles from './adminhome.module.scss'
+import TopSales from "./TopSales";
+
+const filterSales = [
+  {title: 'This week', value: 'week'},
+  {title: 'This month', value: 'month'}
+]
+
 export default function AdminHome(){
   return(
     <>
       <div className={styles.wrapper}>
-        <SalesStatisticTable />
+        <SalesStatisticTable filterSales={filterSales}/>
+        <TopSales filterSales={filterSales}/>
       </div>
     </>
   )
