ce6fbb74bbb6d457721657f640975bb46d854e8c
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-28 20:02:37 +0700

    add title text and hr

--- src/app/(admin)/secret/admin/(main)/SalesStatisticTable.tsx
+++ src/app/(admin)/secret/admin/(main)/SalesStatisticTable.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { useEffect, useState } from "react";
 import AdminCard from "@/component/AdminCard/AdminCard";
 import BarChart from "@/component/BarChart/BarChart";
-import { ReportProps } from "@/component/types/types";
+
 import AxiosGetWithToken from "@/component/utils/axios";
-import { useEffect, useState } from "react";
+import { ReportProps } from "@/component/types/types";
+
+import styles from './adminhome.module.scss'
+import { poppins } from "@/component/fonts/fonts";
 
 export default function SalesStatisticTable() {
   const [reports, setReports] = useState<ReportProps[]>([]);
@@ -16,12 +20,13 @@ export default function SalesStatisticTable() {
       }
     });
   }, []);
-
   return (
-    <AdminCard>
-      { (
+    <AdminCard className={styles.statisticTable}>
+      <h3 className={`${poppins.className} ${styles.title}`}>Product Sold</h3>
+      <hr />
+      <div className={styles.tableContainer}>
         <BarChart title="Sales Report" datas={reports.map(report => {return {label: report.created_at, value: report.income, totalItem: report.total}})} />
-      )}
+      </div>
     </AdminCard>
   );
 }
