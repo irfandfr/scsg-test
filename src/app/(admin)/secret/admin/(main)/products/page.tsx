adccfa4d4dd589c5d5f83a584b02c858bdb77bb6
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-29 01:19:36 +0700

    add button

--- src/app/(admin)/secret/admin/(main)/products/page.tsx
+++ src/app/(admin)/secret/admin/(main)/products/page.tsx
@@ -1,8 +1,10 @@
+import Button from "@/component/Button/Button";
 import ProductTable from "./ProductsTable";
 
 export default function ProductsPage(){
    return(
     <>
+      <Button text="+ Add New Product" style={{marginBottom: '12px'}}/>
       <ProductTable />
     </>
    )
