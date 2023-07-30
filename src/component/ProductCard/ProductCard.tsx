afb543397899a968715fdb288206d8cd3e9ee4ca
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-31 01:01:40 +0700

    add new icon on product card

--- src/component/ProductCard/ProductCard.tsx
+++ src/component/ProductCard/ProductCard.tsx
@@ -11,16 +11,33 @@ interface ProductCardProp{
   created_at: string
 }
 
-export default function ProductCard({id,image,category,name,price}:ProductCardProp){
+export default function ProductCard({id,image,category,name,price, created_at}:ProductCardProp){
+  
+  function isNew(){
+    let date = new Date(created_at)
+    let component = <span style={{width: '6px'}}></span>
+    if(date instanceof Date ){
+      let diffDays = Math.floor(Math.abs((new Date()).getTime() - date.getTime()) / (1000 * 3600 * 24))
+      if(diffDays < 7){//if created less than a week classified as new
+        component = <span className={styles.new}></span>
+      }
+    }
+    return component
+  }
   return(
     <Link
       href={`/product/${id}`}
       className={styles.productCard}
       style={{backgroundImage :`url(${image})`}}
     >
-      <span className={styles.category}>{category}</span>
-      <span className={styles.name}>{name}</span>
-      <span className={styles.price}>$ {parseInt(`${price}`).toLocaleString(undefined,{minimumFractionDigits:2})} /pc</span>
+      <div className={styles.overlay}>
+        <span className={styles.category}>
+          {isNew()}
+          {category}
+        </span>
+        <span className={styles.name}>{name}</span>
+        <span className={styles.price}>$ {parseInt(`${price}`).toLocaleString(undefined,{minimumFractionDigits:2})} /pc</span>
+      </div>
     </Link>
   )
 }
