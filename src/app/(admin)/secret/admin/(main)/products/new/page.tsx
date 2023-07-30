d8620b2f25b0e0799a214306197d3fae937dc1fc
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-30 13:29:12 +0700

    add fetch category

--- src/app/(admin)/secret/admin/(main)/products/new/page.tsx
+++ src/app/(admin)/secret/admin/(main)/products/new/page.tsx
@@ -1,11 +1,25 @@
 import styles from './newproduct.module.scss'
 import ProductForm from './ProductForm'
 
-export default function NewItemPage(){
+async function getCategoryData() {
+  let myHeader = new Headers({})
+  myHeader.append('token', `${process.env.NEXT_PUBLIC_TOKEN}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DB_URL}/platform/product/categories`, {
+    method: 'GET',
+    headers: myHeader
+  })
+  if (!res.ok) {
+    return res.text().then(text => { throw new Error(text) })
+  }
+  return res.json()
+}
+
+export default async function NewItemPage(){
+  const categories = await getCategoryData()
   return(
     <div className={styles.wrapper}>
-      <h2 className={styles.title}>Add new Product</h2>
-      <ProductForm />
+      <h2 className={styles.title}>Add New Product</h2>
+      <ProductForm categories={categories.response}/>
     </div>
   )
 }
