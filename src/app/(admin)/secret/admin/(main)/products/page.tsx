b6c5d59be129e650ee9f03e3ae9222d69a3d8a83
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-30 09:33:00 +0700

    add link to new asset page

--- src/app/(admin)/secret/admin/(main)/products/page.tsx
+++ src/app/(admin)/secret/admin/(main)/products/page.tsx
@@ -4,7 +4,7 @@ import ProductTable from "./ProductsTable";
 export default function ProductsPage(){
    return(
     <>
-      <Button text="+ Add New Product" style={{marginBottom: '12px'}}/>
+      <Button link href="products/new" text="+ Add New Product" style={{marginBottom: '12px'}}/>
       <ProductTable />
     </>
    )
