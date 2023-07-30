bfde58bbeb9e61b6fc8e121a4d62ec1514b135fa
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-30 09:31:55 +0700

    add textform

---
+++ src/component/TextForm/TextForm.tsx
@@ -0,0 +1,43 @@
+import styles from './textform.module.scss'
+
+interface TextFormProp {
+  name?: string
+  id?:string
+  value?: number | string;
+  defaultValue?: number | string
+  type?: 'text' | 'number'
+  label?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>
+    ) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export default function TextForm({
+  value,
+  name,
+  id,
+  defaultValue,
+  label,
+  onChange,
+  type,
+  className,
+  placeholder
+}: TextFormProp) {
+  return (
+    <div className={`${className} ${styles.formContainer}`}>
+      {!!label && label !== "" && <label className={styles.label}>{label}</label>}
+      <input
+        type={!type ? 'text' : type}
+        className={`${styles.textForm}`}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+        defaultValue={defaultValue}
+        min={0}
+        name={name}
+        id={id}
+      />
+    </div>
+  );
+}
