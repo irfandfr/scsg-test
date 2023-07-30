272aed8180700ab66ff9aea40ac74538472c820d
Irfandhia Firdaus <irfandfr@gmail.com>
2023-07-30 17:50:35 +0700

    add product detail component

--- src/component/types/types.ts
+++ src/component/types/types.ts
@@ -19,3 +19,17 @@ export interface ProductProp{
   price: number
   image: string
 }
+
+export interface ProductDataProp{
+  id: string,
+  name: string 
+  description : string
+  sku: string
+  stock: number
+  category_id: number
+  price: number
+  image: string
+  user_id: number
+  created_at: string
+  updated_at: string
+}
